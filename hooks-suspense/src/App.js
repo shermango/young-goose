a04228e90cc5e3ff3c49eeb5d12354556b7af208
Sherman Chen <shc410@gmail.com>
2018-10-30 22:31:45 -0700

    (chore) useEffect provides a callback which can keep track of side effects and only rerender on an array of given hook states changed

--- hooks-suspense/src/App.js
+++ hooks-suspense/src/App.js
@@ -1,10 +1,18 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function withCounter(initialState = 0, step = 1) {
-  const [count, setCount] = useState(initialState);
+  const initialCount = () =>
+    parseInt(window.localStorage.getItem('count') || 0);
+  const [count, setCount] = useState(initialCount);
   const increment = () => setCount(count + step);
+  useEffect(
+    () => {
+      window.localStorage.setItem('count', count);
+    },
+    [count]
+  );
   return { count, increment };
 }
 
@@ -13,17 +21,11 @@ function Counter() {
   return <button onClick={increment}>{count}</button>;
 }
 
-function AnotherCounter() {
-  const { count, increment } = withCounter(5, 3);
-  return <button onClick={increment}>{count}</button>;
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Counter />
-        <AnotherCounter />
       </div>
     );
   }
