3b01653506f7d418f18f29df4cb6c6fbc30535da
Sherman Chen <shc410@gmail.com>
2018-11-04 23:51:32 -0800

    (chore) add in next/prev linking

--- old-sport/src/templates/blogPost.js
+++ old-sport/src/templates/blogPost.js
@@ -1,6 +1,49 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
-const Template = props => <div>Blog Post Here</div>;
+const Template = ({ data, pageContext }) => {
+  const { prev, next } = pageContext;
 
+  console.log(next);
+
+  const { markdownRemark } = data;
+  const title = markdownRemark.frontmatter.title;
+  const html = markdownRemark.html;
+
+  return (
+    <div>
+      <h1 style={{ fontFamily: 'avenir' }}>{title}</h1>
+      <div
+        className="blogPost"
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={{ fontFamily: 'avenir' }}
+      />
+      <div style={{ marginBottom: '1rem', fontFamily: 'avenir' }}>
+        {next && (
+          <Link to={next.frontmatter.path}>
+            Next: {`${next.frontmatter.title}`}
+          </Link>
+        )}
+      </div>
+      <div style={{ fontFamily: 'avenir' }}>
+        {prev && (
+          <Link to={prev.frontmatter.path}>
+            Prev: {`${prev.frontmatter.title}`}
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
 export default Template;
